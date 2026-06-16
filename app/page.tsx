3227130a5e1cbf0b86df5d82a3412ad8e0d0dbe8
tarakeshp07 <tarakeshman@gmail.com>
2026-06-16 10:39:48 +0530

    feat: implement landing page with GoodsReceiptNoteFinal component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import GoodsReceiptNoteFinal from '@/components/GoodsReceiptNoteFinal';
+import GoodsReceiptNoteForm from '@/components/GoodsReceiptNoteForm';
+import GoodsReceiptNoteForm2 from '@/components/GoodsReceiptNoteForm2';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 p-6">
       <GoodsReceiptNoteFinal />
+      {/* <GoodsReceiptNoteForm /> */}
+      {/* <GoodsReceiptNoteForm2 /> */}
     </main>
   );
 }
